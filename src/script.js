61d79f23f1e0307f4c6a7615e7ba06214d0af25f
Lingqiang He <37253359+holytpk@users.noreply.github.com>
2023-02-28 16:46:30 -0500

    1

--- src/script.js
+++ src/script.js
@@ -10,11 +10,6 @@
  *  @returns {*}
  *  @constructor
  */
- if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
-	window.location.href = "../mobile/index_mobile.html");
-} else {
-	window.location.href = "../index.html")
-}
  
 const DOMReady = ((
     callback = () => { },
