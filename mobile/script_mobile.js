4ef1e3680d805316187fc109e36aa764671dc623
Lingqiang He <37253359+holytpk@users.noreply.github.com>
2023-02-28 15:57:36 -0500

    1

--- mobile/script_mobile.js
+++ mobile/script_mobile.js
@@ -1,3 +1,9 @@
+ if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))){
+	window.location.href = "../mobile/index_mobile.html");
+} else {
+	window.location.href = "../index.html")
+}
+
 var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   direction: "horizontal",
